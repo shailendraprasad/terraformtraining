d2b0bf3f6098d418ee5306a654549f97a53a97e8
shailendraprasad <n.shailendraprasad@gmail.com>
2021-04-06 19:55:42 -0700

    Adding s3 module and modified to TF module instead of local

--- Day2/Modules/dev/main.tf
+++ Day2/Modules/dev/main.tf
@@ -1,7 +1,14 @@
 module "ec2" {
-  source = "../ec2module"
+  source  = "shailendraprasad/ec2module/nsp"
+  version = "1.0.0"
   instanceCount = 1
   tags = {
       ENV : "DEV"
   }
+}
+  
+
+module "s3" {
+  source = "../s3module"
+  bucket_name = "bucketnamedev-nsp"
 }
---
+++ Day2/Modules/s3module/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "mys3" {
+    bucket = var.bucket_name
+}
